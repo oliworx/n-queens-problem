6f544fa5d8dfdd3b93a03dc7496a43be7d0280fd
Oliver Kurmis <oliver@kurmis.com>
2018-04-08 22:53:25 +0200

    Refactor to generalize (n-queens)

--- n-queens.py
+++ n-queens.py
@@ -1,26 +1,28 @@
-dame = [0,0,0,0]
-zeile = [0,0,0,0]
-diag1 = [0,0,0,0,0,0,0]
-diag2 = [0,0,0,0,0,0,0]
+N = 4
+dame = [0]*N
+zeile = [0]*N
+diag1 = [0]*(2*N-1)
+diag2 = [0]*(2*N-1)
 
-def setzedame(n, zeile, diag1, diag2):
+def setzedame(n):
     pos = 0
-    while pos < 4:
+    while pos < N:
         d1=n+pos
         d2=pos-n+3
         if zeile[pos] == 0 and  diag1[d1] == 0 and diag2[d2] == 0 :
             dame[n] = pos
-            if n < 3 :
+            if n == N-1 :
+                print('solution:')
+                print(dame)
+            else:
                 zeile[pos]=1
                 diag1[d1] = 1
                 diag2[d2]=1
-                setzedame(n+1, zeile[:], diag1[:], diag2[:])
+                setzedame(n+1)
                 zeile[pos]=0
                 diag1[d1] = 0
                 diag2[d2]=0
-            else:
-                print('solution:')
-                print(dame)
+
         pos = pos + 1
 
-setzedame(0, zeile, diag1, diag2)
+setzedame(0)
