a0271f3cf02620e296753bfcc8762ea1d1ed23a1
Oliver Kurmis <oliver@kurmis.com>
2018-04-08 23:25:56 +0200

    8 queens

--- n-queens.py
+++ n-queens.py
@@ -1,10 +1,12 @@
-N = 4
+N = 8
 dame = [0]*N
 zeile = [0]*N
 diag1 = [0]*(2*N-1)
 diag2 = [0]*(2*N-1)
+solutions = 0
 
 def setzedame(n):
+    global solutions
     pos = 0
     while pos < N:
         d1=n+pos
@@ -12,8 +14,8 @@ def setzedame(n):
         if zeile[pos] == 0 and  diag1[d1] == 0 and diag2[d2] == 0 :
             dame[n] = pos
             if n == N-1 :
-                print('solution:')
-                print(dame)
+                print('.', end='')
+                solutions += 1
             else:
                 zeile[pos]=1
                 diag1[d1] = 1
@@ -23,6 +25,7 @@ def setzedame(n):
                 diag1[d1] = 0
                 diag2[d2]=0
 
-        pos = pos + 1
+        pos += 1
 
 setzedame(0)
+print(solutions)
