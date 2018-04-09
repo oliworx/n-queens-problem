09f775e69404b4461ae775d2fdfd82217b350d47
Oliver Kurmis <oliver@kurmis.com>
2018-04-10 00:30:17 +0200

    9 queens and optimize

--- n-queens.py
+++ n-queens.py
@@ -1,31 +1,29 @@
-N = 8
-dame = [0]*N
-zeile = [0]*N
-diag1 = [0]*(2*N-1)
-diag2 = [0]*(2*N-1)
-solutions = 0
+N = 9
+queens = bytearray([0]*N)
+row    = bytearray([1]*N)
+diag1  = bytearray([1]*(2*N-1))
+diag2  = bytearray([1]*(2*N-1))
 
 def setzedame(n):
-    global solutions
+    solutions = 0
     pos = 0
     while pos < N:
         d1=n+pos
         d2=pos-n+3
-        if zeile[pos] == 0 and  diag1[d1] == 0 and diag2[d2] == 0 :
-            dame[n] = pos
+        if row[pos] and diag1[d1] and diag2[d2] :
+            queens[n] = pos
             if n == N-1 :
-                print('.', end='')
                 solutions += 1
             else:
-                zeile[pos]=1
-                diag1[d1] = 1
-                diag2[d2]=1
-                setzedame(n+1)
-                zeile[pos]=0
+                row[pos]  = 0
                 diag1[d1] = 0
-                diag2[d2]=0
+                diag2[d2] = 0
+                solutions += setzedame(n+1)
+                row[pos]  = 1
+                diag1[d1] = 1
+                diag2[d2] = 1
 
         pos += 1
-
-setzedame(0)
-print(solutions)
+    return solutions
+    
+print(setzedame(0))
