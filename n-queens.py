9d513c1e37b3f96949187078266314f478b2e52d
Oliver Kurmis <oliver@kurmis.com>
2018-04-10 23:37:40 +0200

    10 queens and optimize algorithm
    Due to symmetry the first queen only has to walk the first half of the board.

--- n-queens.py
+++ n-queens.py
@@ -1,10 +1,21 @@
-N = 9
+N = 10
 queens = bytearray([0]*N)
 row    = bytearray([1]*N)
 diag1  = bytearray([1]*(2*N-1))
 diag2  = bytearray([1]*(2*N-1))
 
-def setzedame(n):
+def next_queen(n, pos, d1, d2):
+    row[pos]  = 0
+    diag1[d1] = 0
+    diag2[d2] = 0
+    solutions = move_queen(n+1)
+    row[pos]  = 1
+    diag1[d1] = 1
+    diag2[d2] = 1
+
+    return solutions
+                
+def move_queen(n):
     solutions = 0
     pos = 0
     while pos < N:
@@ -15,15 +26,17 @@ def setzedame(n):
             if n == N-1 :
                 solutions += 1
             else:
-                row[pos]  = 0
-                diag1[d1] = 0
-                diag2[d2] = 0
-                solutions += setzedame(n+1)
-                row[pos]  = 1
-                diag1[d1] = 1
-                diag2[d2] = 1
+                solutions += next_queen(n, pos, d1, d2)
 
         pos += 1
     return solutions
-    
-print(setzedame(0))
+
+
+solutions = 0
+pos = 0
+while pos < N/2:
+    queens[0] = pos
+    solutions += next_queen(0, pos, pos, pos+3)
+    pos += 1
+
+print(solutions*2)
